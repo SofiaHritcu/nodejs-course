1a3e8f143847164a381686d6972c1cc8b8d16c7b
SofiaHritcu <61913547+SofiaHritcu@users.noreply.github.com>
2022-02-03 22:37:06 +0200

    node: use controllers for blogs

---
+++ 02_servers/controllers/blogController.js
@@ -0,0 +1,61 @@
+const Blog = require('../models/blog');
+
+const blog_index = (req, res) => {
+    Blog.find()
+        .sort({ createdAt: -1 })
+        .then((result) => {
+            res.render('blogs/index', {title: 'BLOGS', blogs: result});
+        })
+        .catch((e) => {
+            console.log(e);
+        });
+};
+
+const blog_details = (req, res) => {
+    const id = req.params.id;
+    Blog.findById(id)
+        .then((result) => {
+            res.render('blogs/details', {blog: result, title: 'BLOG DETAILS'});
+        })
+        .catch((e) => {
+            console.log(e);
+        });
+};
+
+const blog_create_get = (req, res) => {
+    res.render('blogs/create', { title: 'CREATE BLOG' });
+};
+
+const blog_create_post = (req, res) => {
+    const blog = new Blog(req.body);
+    blog.save()
+        .then(() => {
+            res.redirect('/blogs');
+        })
+        .catch((e) => {
+            console.log(e);
+        });
+};
+
+const blog_delete = (req, res) => {
+    const id = req.params.id;
+    Blog.findByIdAndDelete(id)
+        .then((result) => {
+            // when coming from an AJAX request
+            // in node we cannot just redirect
+            // we have to send back an answer -> JSON object
+            // the JSON object will have a redirect property
+            res.json({ redirect: '/blogs' });
+        })
+        .catch((e) => {
+            console.log(e);
+        });
+};
+
+module.exports = {
+    blog_index,
+    blog_details,
+    blog_create_get,
+    blog_create_post,
+    blog_delete,
+}
--- 02_servers/routes/blogRoutes.js
+++ 02_servers/routes/blogRoutes.js
@@ -1,59 +1,13 @@
 const express = require('express');
-const Blog = require('../models/blog');
+const blogController = require('../controllers/blogController');
 
 const router = express.Router();
 
 // blog routes
-router.get('/create', (req, res) => {
-    res.render('create', { title: 'CREATE BLOG' });
-});
-
-router.get('', (req, res) => {
-    Blog.find()
-        .sort({ createdAt: -1 })
-        .then((result) => {
-            res.render('index', {title: 'BLOGS', blogs: result});
-        })
-        .catch((e) => {
-            console.log(e);
-        });
-});
-
-router.post('/', (req, res) => {
-    const blog = new Blog(req.body);
-    blog.save()
-        .then(() => {
-            res.redirect('/blogs');
-        })
-        .catch((e) => {
-            console.log(e);
-        });
-});
-
-router.get('/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findById(id)
-        .then((result) => {
-            res.render('details', {blog: result, title: 'BLOG DETAILS'});
-        })
-        .catch((e) => {
-            console.log(e);
-        });
-});
-
-router.delete('/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findByIdAndDelete(id)
-        .then((result) => {
-            // when coming from an AJAX request
-            // in node we cannot just redirect
-            // we have to send back an answer -> JSON object
-            // the JSON object will have a redirect property
-            res.json({ redirect: '/blogs' });
-        })
-        .catch((e) => {
-            console.log(e);
-        });
-});
+router.get('/create', blogController.blog_create_get);
+router.get('/', blogController.blog_index);
+router.post('/', blogController.blog_create_post);
+router.get('/:id', blogController.blog_details);
+router.delete('/:id', blogController.blog_delete);
 
 module.exports = router;
